6bdf11a77911907cccaa57de8acb244528c45419
vijayjavaprgmr <vijayjavaprgmr@gmail.com>
2021-01-19 18:09:12 +0530

    Code Refactor: extracted object initilization for reusability

--- src/test/java/BerlinTimeConverterTest.java
+++ src/test/java/BerlinTimeConverterTest.java
@@ -1,21 +1,27 @@
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.bnpp.kata.BerlinTimeConverter;
 
 public class BerlinTimeConverterTest {
 
+	BerlinTimeConverter berlinTimeConverter;
+
+	@Before
+	public void initializeBerlinTimeConverterObj() {
+		berlinTimeConverter = new BerlinTimeConverter();
+	}
+
 	@Test
 	public void testBottomMinutesShouldReturnRowAs_OOOO() {
-		BerlinTimeConverter berlinTimeConverter = new BerlinTimeConverter();
 		assertThat(berlinTimeConverter.getBottomMinutes("00:00:00"), is("OOOO"));
 	}
 
 	@Test
 	public void testBottomMinutesShouldReturnRowAs_YYYY() {
-		BerlinTimeConverter berlinTimeConverter = new BerlinTimeConverter();
 		assertThat(berlinTimeConverter.getBottomMinutes("23:59:59"), is("YYYY"));
 	}
 
