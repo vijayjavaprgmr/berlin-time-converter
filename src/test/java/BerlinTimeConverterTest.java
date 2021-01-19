fd01e35a7ee9029175ca25738c5cd11113bf2637
vijayjavaprgmr <vijayjavaprgmr@gmail.com>
2021-01-19 18:01:53 +0530

    Berlin Time should return bottom minutes as 'YYYY' for the give time

--- src/test/java/BerlinTimeConverterTest.java
+++ src/test/java/BerlinTimeConverterTest.java
@@ -13,4 +13,10 @@ public class BerlinTimeConverterTest {
 		assertThat(berlinTimeConverter.getBottomMinutes("00:00:00"), is("OOOO"));
 	}
 
+	@Test
+	public void testBottomMinutesShouldReturnRowAs_YYYY() {
+		BerlinTimeConverter berlinTimeConverter = new BerlinTimeConverter();
+		assertThat(berlinTimeConverter.getBottomMinutes("23:59:59"), is("YYYY"));
+	}
+
 }
