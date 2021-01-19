ae300a5058ff8a06848a2748e76e845b60c090cd
vijayjavaprgmr <vijayjavaprgmr@gmail.com>
2021-01-19 20:10:16 +0530

    Code Refactor: extracted code for reusability

--- src/main/java/com/bnpp/kata/BerlinTimeConverter.java
+++ src/main/java/com/bnpp/kata/BerlinTimeConverter.java
@@ -5,29 +5,34 @@ import java.util.Arrays;
 public class BerlinTimeConverter {
 
 	public String getBottomMinutes(String berlinTime) {
-		int[] berlinTimeExtract = Arrays.asList(berlinTime.split(":")).stream().mapToInt(Integer::parseInt).toArray();
-		StringBuilder outSignal = new StringBuilder();
+		int[] berlinTimeExtract = getTimeExtract(berlinTime);
+		StringBuilder outBottomMinutes = new StringBuilder();
 		int onSigns = berlinTimeExtract[1] % 5;
-		for (int i = 0; i < onSigns; i++) {
-			outSignal.append("Y");
-		}
-		for (int j = 0; j < (4 - onSigns); j++) {
-			outSignal.append("O");
-		}
-		return outSignal.toString();
+		outBottomMinutes.append(getOnOff(4, onSigns, "Y"));
+		return outBottomMinutes.toString();
 	}
 
 	public String getTopMinutes(String berlinTime) {
-		int[] berlinTimeExtract = Arrays.asList(berlinTime.split(":")).stream().mapToInt(Integer::parseInt).toArray();
-		StringBuilder outSignal = new StringBuilder();
+		int[] berlinTimeExtract = getTimeExtract(berlinTime);
+		StringBuilder outTopMinutes = new StringBuilder();
 		int onSigns = (berlinTimeExtract[1] - (berlinTimeExtract[1] % 5)) / 5;
+		outTopMinutes.append(getOnOff(11, onSigns, "Y"));
+		return outTopMinutes.toString().replace("YYY", "YYR");
+	}
+
+	private String getOnOff(int lamps, int onSigns, String onSign) {
+		StringBuilder outsign = new StringBuilder();
 		for (int i = 0; i < onSigns; i++) {
-			outSignal.append("Y");
+			outsign.append(onSign);
 		}
-		for (int j = 0; j < (11 - onSigns); j++) {
-			outSignal.append("O");
+		for (int j = 0; j < (lamps - onSigns); j++) {
+			outsign.append("O");
 		}
-		return outSignal.toString().replace("YYY", "YYR");
+		return outsign.toString();
+	}
+
+	private int[] getTimeExtract(String berlinTime) {
+		return Arrays.asList(berlinTime.split(":")).stream().mapToInt(Integer::parseInt).toArray();
 	}
 
 }
